a9e5d12cd313a6c32c4f410edd4a88e5a377ab5f
javierhemartinez-droid <javierhemartinez@gmail.com>
2026-05-18 10:56:36 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'barreta-v1';
+const ASSETS = [
+  '/Agenda-Barreta/agenda-barreta.html',
+  '/Agenda-Barreta/manifest.json',
+  '/Agenda-Barreta/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
